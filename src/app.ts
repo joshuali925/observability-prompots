51a39518a621ad2030120b9adf2573c64df0127b
Shenoy Pratik <sgguruda@amazon.com>
2023-05-10 10:54:46 -0700

    adding agent example
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>

--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,39 @@
-// import { OpenAI } from "langchain/llms/openai";
-// import { initializeAgentExecutorWithOptions } from "langchain/agents";
-// import { DynamicTool } from "langchain/tools";
-// import { sql_tool } from "./tool.js";
-// import { ConsoleCallbackHandler } from "langchain/callbacks";
-
-// export const run = async () => {
-//   const model = new OpenAI({ temperature: 0 });
-
-//   const pplQuery = `source=opensearch_dashboards_sample_data_logs | where timestamp >= '2023-05-09 17:49:46' and timestamp <= '2023-05-09 18:04:46' | stats count() by host`;
-
-//   const tools = [
-//     new DynamicTool({
-//       name: "PPL Query Tool",
-//       description: "use this tool to make a PPL Query in an OpenSearch cluster",
-//       func: () => sql_tool(pplQuery),
-//     }),
-//   ];
-
-//   const executor = await initializeAgentExecutorWithOptions(tools, model, {
-//     agentType: "zero-shot-react-description",
-//     verbose: true,
-//     callbacks: [new ConsoleCallbackHandler()], // add a console callback handler
-//   });
-
-//   console.log("Loaded agent.");
-
-//   const input = `Can you execute a PPL query and summarize the response?`;
-
-//   console.log(`Executing with input "${input}"...`);
-
-//   const result = await executor.call({ input });
-
-//   console.log(`Got output ${result.output}`);
-// };
-// run();
-// console.log("Welcome to the LangChain.js tutorial by LangChainers.");
-
 import { OpenAI } from "langchain/llms/openai";
+import { initializeAgentExecutorWithOptions } from "langchain/agents";
+import { DynamicTool } from "langchain/tools";
+import { sql_tool } from "./tool.js";
+import { ConsoleCallbackHandler } from "langchain/callbacks";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
-const model = new OpenAI({
-  temperature: 0.9,
-  modelName: "gpt-3.5-turbo",
-  openAIApiKey: "sk-nMUTm9o0r9Ly7oAaaUqTT3BlbkFJgZxEcW0iuCXzmOoO54b", // In Node.js defaults to process.env.OPENAI_API_KEY
-});
+export const run = async () => {
+  const model = new OpenAI({ temperature: 0 });
 
-const res = await model.call(
-  "What would be a good company name a company that makes colorful socks?"
-);
-console.log({ res });
+  const tools = [
+    new DynamicTool({
+      name: "PPL Query Tool",
+      description:
+        "use this tool to execute a PPL Query in an OpenSearch cluster",
+      func: () => sql_tool(),
+    }),
+  ];
+
+  const executor = await initializeAgentExecutorWithOptions(tools, model, {
+    agentType: "zero-shot-react-description",
+    verbose: true,
+    callbacks: [new ConsoleCallbackHandler()], // add a console callback handler
+    maxIterations: 2,
+  });
+
+  console.log("Loaded agent.");
+
+  const input = `Can you execute a PPL query and summarize the response?`;
+
+  console.log(`Executing with input "${input}"...`);
+
+  const result = await executor.call({ input });
+
+  console.log(`Got output ${result.output}`);
+};
+run();
+// console.log("Welcome to the LangChain.js tutorial by LangChainers.");
--- src/tool.ts
+++ src/tool.ts
@@ -1,13 +1,26 @@
-// import axios from "axios";
+import axios from "axios";
 
-// export const sql_tool = async (pplQuery: string) => {
-//   await axios
-//     .post("http://localhost:5601/api/ppl/search", {
-//       query: pplQuery,
-//       format: "jdbc",
-//     })
-//     .then((res) => {
-//       return JSON.stringify(res);
-//     });
-//   return "Error while making a request for PPL Query";
-// };
+export const sql_tool = async () => {
+  const pplQuery = `source=opensearch_dashboards_sample_data_logs | where timestamp >= '2023-05-09 17:49:46' and timestamp <= '2023-05-09 18:04:46' | stats count() by host`;
+  // console.log("got pplQuery ################", pplQuery);
+  await axios.default
+    .post(
+      "http://127.0.0.1:5601/api/ppl/search",
+      {
+        query: pplQuery,
+        format: "jdbc",
+      },
+      {
+        headers: { "osd-xsrf": true },
+        auth: {
+          username: process.env.OS_USERNAME || "",
+          password: process.env.OS_PASSWORD || "",
+        },
+      }
+    )
+    .then((res: any) => {
+      // console.log("got response hjere ################");
+      return JSON.stringify(res.jsonData);
+    });
+  return "Error while making a request for PPL Query";
+};
