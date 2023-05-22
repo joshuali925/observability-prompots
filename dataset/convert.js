e562e03c8b5f8cd49295ab18c97384e7495fbe21
Joshua Li <joshuali925@gmail.com>
2023-05-22 21:30:40 +0000

    --wip--
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

---
+++ dataset/convert.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+
+const dataDir = `${__dirname}/data`;
+
+const writeFile = (obj) => {
+  fs.writeFileSync(`${dataDir}/ppl_samples.json`, JSON.stringify(obj, null, 2));
+};
+
+const lines = fs
+  .readFileSync(`${dataDir}/jaeger.txt`, "utf8")
+  .split(/\r?\n/)
+  .filter((line) => line.trim() !== "");
+
+const samples = [];
+
+tag = "jaeger-span";
+
+context = {
+  "jaeger-span": `
+index name: 'jaeger-span-*'
+
+field for error: 'tag.error'
+field for latency, response time, duration: 'duration'
+field for spans: 'spanId'
+field for service: 'process.serviceName'
+
+Accounting Service: 'accountingservice'
+Ad Service: 'adservice'
+Cache: 'cache'
+Cart Service: 'cartservice'
+Checkout Service: 'checkoutservice'
+Currency Service: 'currencyservice'
+Email Service: 'emailservice'
+Fraud Detection Service: 'frauddetectionservice'
+Frontend: 'frontend'
+Frontend Proxy: 'frontendproxy'
+Load Generator: 'loadgenerator'
+Payment Service: 'paymentservice'
+Product Catalog Service: 'productcatalogservice'
+Quote Service: 'quoteservice'
+Recommendation Service: 'recommendationservice'
+Shipping Service: 'shippingservice'
+Feature Flag Service: 'featureflagservice'
+Feature Flag Store: 'featureflagstore'
+Queue: 'queue'
+`.trim(),
+};
+
+for (let i = 0; i < lines.length; i += 2) {
+  const description = lines[i];
+  const query = lines[i + 1];
+  samples.push({
+    tag,
+    description,
+    context: context[tag],
+    query,
+  });
+}
+
+console.info("❗samples:", samples);
+
+writeFile(samples)
--- tools_ppl_generator.py
+++ tools_ppl_generator.py
@@ -16,77 +16,55 @@ You will be given a question about some metrics from a user.
 Use context provided to write a PPL query that can be used to retrieve the information.
 
 ----------------
-Here are some sample questions and the PPL query to retrieve the information. Format:
+Here are some sample questions and the PPL query to retrieve the information.
 
-a. Question
-b. PPL query with sample schema
-c. PPL query with placeholder variables
+Give me some documents in index 'accounts'
+source=`accounts`
 
-[index] is a placeholder for index name.
-[field] is a placeholder for a field name.
-[value] is a placeholder for a field value.
+Give me 10 documents in index 'accounts'
+source=`accounts` | head 10
 
-Examples:
+Give me 5 oldest people in index 'accounts'
+source=`accounts` | sort - age | head 5
 
-a. What is the throughput of each service?
-b. source=jaeger-span* | stats count() by process.serviceName
-c. source=[index] | stats count() by [field]
+Give me first names of 5 youngest people in index 'accounts'
+source=`accounts` | sort age | head 5 | fields `firstname`
 
-a. What is the number of spans of service 'loadgenerator'?
-b. source=jaeger-span* | where process.serviceName = 'loadgenerator' | stats count()
-c. source=[index] | where [field] = '[value]' | stats count()
+Give me some addresses in index 'accounts'. field for addresses is 'address'
+source=`accounts` | fields `address`
 
-a. What is the number of spans of service load generator per second?
-b. source=jaeger-span* | where process.serviceName = 'loadgenerator' | stats count() by span('startTime', 1s)
-c. source=[index] | where [field] = '[value]' | stats count() by span('[field]', 1s)
+Find the document in index 'accounts' where firstname is 'Hattie'
+source=`accounts` | where `firstname` = 'Hattie'
 
-a. What is the average latency of spans in each service?
-b. source=jaeger-span* | stats avg(duration) by process.serviceName
-c. source=[index] | stats avg([field]) by [field]
+Find the document in index 'accounts' where firstname is 'Hattie' or lastname is 'Frank'
+source=`accounts` | where `firstname` = 'Hattie' or `lastname` = 'frank'
 
-a. What is the current average latency of spans in each service?
-b. source=jaeger-span* | where startTime >= 'now-5m' | stats avg(duration) by process.serviceName
-c. source=[index] | where [field] >= 'now-5m' | stats avg([field]) by [field]
+Find the document in index 'accounts' where firstname is not 'Hattie' and lastname is not 'Frank'
+source=`accounts` | where `firstname` != 'Hattie' and `lastname` != 'frank'
 
-a. What is the average latency of spans by service and operation name?
-b. source=jaeger-span* | stats avg(duration) by process.serviceName, operationName
-c. source=[index] | stats avg([field]) by [field], [field]
+Count the number of documents in index 'accounts'
+source=`accounts` | stats count()
 
-a. What is the average latency of spans in every 5 minutes intervals?
-b. source=jaeger-span* | stats avg(duration) by span('startTime', 5m)
-c. source=[index] | stats avg([field]) by span('[field]', 5m)
+Count the number of people with firstname 'Amber' in index 'accounts'
+source=`accounts` | where `firstname`='Amber' | stats count()
 
-a. What is the average latency of spans of service `frontend`?
-b. source=jaeger-span* | where process.serviceName = 'frontend' | stats avg(duration)
-c. source=[index] | where [field] = '[value]' | stats avg([field])
+How many people are older than 33? index is 'accounts', age fields is 'age'
+source=`accounts` | where `age` > 33 | stats count()
 
-a. What are some services with latency over 1 second?
-b. source=jaeger-span* | where duration > 1000000 | stats count() by process.serviceName
-c. source=[index] | where [field] > 1000000 | stats count() by [field]
+How many males and females in index 'accounts'? gender fields is 'gender'
+source=`accounts` | stats count() by `gender`
 
-a. What are some spanIDs with latency over 1 second for the load generator service
-b. source=jaeger-span* | where duration > 1000000 and process.serviceName = 'loadgenerator' | fields spanID
-c. source=[index] | where [field] > 1000000 and [field] = '[value]' | fields [field]
+What is the average age in 'accounts' index?
+source=`accounts` | stats avg(`age`)
 
-a. What are some services with errors?
-b. source=jaeger-span* | where status.code > 0 | stats count() by process.serviceName
-c. source=[index] | where [field] > 0 | stats count() by [field]
+What is the minimum age in 'accounts' index?
+source=`accounts` | stats min(`age`)
 
-a. What are some spans with errors for the accounting service
-b. source=jaeger-span* | where status.code > 0 and process.serviceName = 'accounting' | fields SpanID
-c. source=[index] | where [field] > 0 and [field] = '[value]' | fields [field]
-
-a. What are the top 5 services with errors?
-b. source=jaeger-span* | where status.code > 0 | stats count() as errors by process.serviceName | sort - errors
-c. source=[index] | where [field] > 0 | stats count() as errors by [field] | sort - errors
-
-a. What are the top 5 spans with least latency?
-b. source=jaeger-span* | sort duration | head 5 | fields spanID
-c. source=[index] | sort [field] | head 5 | fields [field]
+What is the maximum age in 'accounts' index?
+source=`accounts` | stats max(`age`)
 
 ---------------
 
-Before you write the query, use tools to replace placeholder values. A tool can only be used once
 You have access to the following tools:
 """
 
@@ -119,4 +97,4 @@ agent_chain = AgentExecutor.from_agent_and_tools(
     memory=memory,
 )
 
-agent_chain.run(input="what is the average latency of Fraud Detection Service?")
+agent_chain.run(input="what is the average latency of Fraud Detection Service? field for latency is 'duration', field for service is 'process.serviceName', value for Fraud Detection Service is 'frauddetectionservice', index is 'jaeger-span-*'")
